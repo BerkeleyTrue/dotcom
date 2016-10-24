f97d8f856321c6de456e8a3b4c111677f23ad7b5
Berkeley Martinez <Berkeley@robotie.com>
2016-10-24 08:01:40 -0700

    fix: gh-pages script should not be silent on fail

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-process-exit */
 const path = require('path');
 const ghpages = require('gh-pages');
-const debugFactory = require('debug');
 
-const log = debugFactory('berkeleytrue:gh-pages');
-
-log('starting gh-pages push');
+console.log('\nstarting gh-pages push\n');
 ghpages.publish(
   path.join(__dirname, 'public'),
   function(err) {
-    if (err) { throw err; }
-    log('gh-pages push completed');
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log('\ngh-pages push completed\n');
     process.exit(0);
   }
 );
