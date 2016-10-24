f5eb60fb0ab3aa13aeda7888796b5f9c75d639d8
Berkeley Martinez <Berkeley@robotie.com>
2016-10-24 06:33:24 -0700

    feat: Add about info

--- client/routes/about/About.jsx
+++ client/routes/about/About.jsx
@@ -16,7 +16,23 @@ export default class About extends PureComponent {
           </h1>
         </header>
         <article>
-          Hey I'm berkeley :)
+          Hello, I'm berkeley Martinez, I work as a developer in
+          San Francisco.
+          <ul>
+            <li>
+              Need to build a fullstack React.js app?
+            </li>
+            <li>
+              Looking to move to the JAM stack?
+            </li>
+            <li>
+              Need some advice node.js advice?
+            </li>
+            <li>
+              Are you a new developer and need a few tutoring sessions?
+            </li>
+          </ul>
+          I can help. Let's talk
         </article>
       </div>
     );
