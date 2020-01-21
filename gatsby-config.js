48c327ebeaa4d31b1249e00fe65a1b0c498ee09f
Berkeley True <berkeleytrue@users.noreply.github.com>
2020-01-20 20:18:26 -0800

    feat(build): add postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'BerkeleyTrue',
     description: 'My personal page',
-    author: '@berkeleytrue'
+    author: '@berkeleytrue',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -10,8 +10,8 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
@@ -23,14 +23,34 @@ module.exports = {
         short_name: 'berkeleytrue',
         start_url: '/',
         background_color: '#663399',
-        theme_color: '#663399',
+        theme_color: '#eadeda',
         display: 'minimal-ui',
         // This path is relative to the root of the site.
-        icon: 'src/images/gatsby-icon.png'
-      }
-    }
+        icon: 'src/images/gatsby-icon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          /* eslint-disable comma-dangle */
+          require('postcss-nested-ancestors')({}),
+          require('postcss-nested')({}),
+          require('postcss-import')({}),
+          require('postcss-flexbox')({}),
+          require('postcss-button')({}),
+          require('postcss-simple-vars')({}),
+          require('postcss-automath')({}),
+          require('postcss-normalize')({}),
+          require('postcss-console')({}),
+          require('autoprefixer')({}),
+          require('cssnano')({}),
+          /* eslint-enable comma-dangle */
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
+  ],
 };
