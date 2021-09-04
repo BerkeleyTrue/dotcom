a77a4c23d5a8368de3939fdadbde163961ff544f
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-09-04 12:19:15 -0700

    build(11ty): make port 3000

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,22 @@ const pluginTailwindCSS = require("eleventy-plugin-tailwindcss");
 // expose to pug
 global.dayjs = dayjs;
 
+/**
+ * Eleventy config
+ * @param {import('@11ty/eleventy/src/UserConfig')} eleventyConfig
+ */
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginTailwindCSS, {
     src: './src/css/**/*.css',
     keepFolderStructure: false,
     watchEleventyWatchTargets: true,
     dest: 'assets',
+  });
+  eleventyConfig.setBrowserSyncConfig({
+    port:3000,
+    open: true,
+    notify: true,
+    ui: { port:3001 },
+
   });
 };
