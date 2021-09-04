8802cb90269d1186c961faed3fb732d574895588
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-09-03 23:04:58 -0700

    feat: list posts on home page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const dayjs = require('dayjs');
 const pluginTailwindCSS = require("eleventy-plugin-tailwindcss");
+
+// expose to pug
+global.dayjs = dayjs;
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginTailwindCSS, {
--- posts/init.md
+++ posts/init.md
@@ -1,11 +1,11 @@
 ---
 title: "Init blog"
+summary: 'A small start'
+date: 2021-03-05T08:00:00.000Z
 ---
 
 ## Init I
 
 I am a blog.
-I start.
-You read.
 
 Fin.
