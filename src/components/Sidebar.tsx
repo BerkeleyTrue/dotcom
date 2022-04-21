314b86dd0f31638ccc0cfcbeeddc094a382e8558
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-21 07:46:06 -0700

    feat(sidebar): update header/items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ import {
   VStack,
   Text,
 } from '@chakra-ui/react';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, ReactNode } from 'react';
 import { Avatar } from './Avatar';
 
 export type Variants = 'drawer' | 'sidebar';
@@ -19,6 +19,24 @@ const sidebarStyles = {
   bg: 'darker.800',
 };
 
+const List = ({ children }: { children: ReactNode }) => (
+  <>
+    <Text as="span" color="cyan.400">
+      (
+    </Text>
+    {children}
+    <Text as="span" color="cyan.400">
+      )
+    </Text>
+  </>
+);
+
+const Func = ({ children }: { children: ReactNode }) => (
+  <Text as="span" color="red.400">
+    {children}
+  </Text>
+);
+
 interface ContentProps {
   onClick: MouseEventHandler;
 }
@@ -26,26 +44,33 @@ const SidebarContent = ({ onClick }: ContentProps) => (
   <VStack>
     <Avatar />
     <Text fontSize={{ base: 'medium' }} mb="4">
-      (= Berkeley true)
+      <List>
+        <Func>Berkeley</Func>
+        <Text as="span" color="purple.400">
+          &nbsp;true
+        </Text>
+      </List>
     </Text>
     <NextLink href="/" passHref>
-      <Button onClick={onClick} w="100%">
-        { '(->' } Home)
+      <Button onClick={onClick} w="100%" display="flex" justifyContent="left">
+        <List>
+          <Func>{'->'}</Func>&nbsp;Home
+        </List>
       </Button>
     </NextLink>
     <NextLink href="/catagories" passHref>
-      <Button onClick={onClick} w="100%">
-        { '(->>' } Catagories)
+      <Button onClick={onClick} w="100%" display="flex" justifyContent="left">
+        <List><Func>{'->>'}</Func>&nbsp;Catagories</List>
       </Button>
     </NextLink>
     <NextLink href="/tags" passHref>
-      <Button onClick={onClick} w="100%">
-        { '(some->' } Tags)
+      <Button onClick={onClick} w="100%" display="flex" justifyContent="left">
+        <List><Func>{'->>'}</Func>&nbsp;Tags</List>
       </Button>
     </NextLink>
     <NextLink href="/about" passHref>
-      <Button onClick={onClick} w="100%" textAlign='left'>
-        { '(as->' } About)
+      <Button onClick={onClick} w="100%" display="flex" justifyContent="left">
+        <List><Func>{'->'}</Func>&nbsp;About</List>
       </Button>
     </NextLink>
   </VStack>
