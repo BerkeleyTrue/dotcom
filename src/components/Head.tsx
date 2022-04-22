a6ec933dd0783ec2343495f81e402ddae84e422c
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-22 15:38:36 -0700

    fix(Head): add favicon and subtitle

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,14 +1,14 @@
 import NextHead from 'next/head';
+import { ReactNode } from 'react';
 
 interface Props {
   subTitle: string;
+  children: ReactNode;
 }
-export const Head = ({ subTitle }: Props) => (
+export const Head = ({ subTitle, children }: Props) => (
   <NextHead>
-    <title>
-
-      berks//{subTitle}
-    </title>
-      <link href='/images/favicon.ico' rel='shortcut icon' />
+    <title>berks//{subTitle}</title>
+    <link href="/images/favicon.ico" rel="shortcut icon" />
+    {children}
   </NextHead>
-)
+);
