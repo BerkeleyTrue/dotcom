0cca3441ced9969641f898f77e919c992637ed39
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-22 21:26:07 -0700

    feat(lib/mdx): add get by slug

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -7,14 +7,19 @@ import { getAllFilesRecursively } from './utils/files';
 
 const root = process.cwd();
 
-export function formatSlug(slug: string) {
-  return slug.replace(/\.(mdx|md)/, '');
-}
-export function sortDateDesc(a: string, b: string) {
-  if (a > b) return -1;
-  if (a < b) return 1;
-  return 0;
-}
+export const formatSlug = _.replace(/\.(mdx|md)/, '');
+
+export const formatFrontmatterData = ([
+  fileName,
+  frontmatter,
+]): FrontMatter => ({
+  ...frontmatter,
+  slug: formatSlug(fileName),
+  date: frontmatter.date ? new Date(frontmatter.date).toISOString() : null,
+  lastEdit: frontmatter.lastEdit
+    ? new Date(frontmatter.date).toISOString()
+    : null,
+});
 
 export interface FrontMatter {
   data: string;
@@ -25,9 +30,30 @@ export interface FrontMatter {
   lastEdit: string;
 }
 
-export async function getAllFilesFrontMatter(
-  folder: string,
-): Promise<FrontMatter[]> {
+export const getFiles = (type: string): string[] => {
+  const prefixPaths = path.join(root, 'data', type);
+
+  const files = getAllFilesRecursively(prefixPaths);
+
+  // Only want to return blog/path and ignore root, replace is needed to work on Windows
+  return files.map((file) =>
+    file.slice(prefixPaths.length + 1).replace(/\\/g, '/'),
+  );
+};
+
+export const getFileBySlug = (type: string, slug: string): FrontMatter => {
+  const mdxPath = path.join(root, 'data', type, `${slug}.mdx`);
+  const mdPath = path.join(root, 'data', type, `${slug}.md`);
+  const source = fs.existsSync(mdxPath)
+    ? fs.readFileSync(mdxPath, 'utf8')
+    : fs.readFileSync(mdPath, 'utf8');
+
+  return _.flow(matter, _.get('data'), (data) =>
+    formatFrontmatterData([slug, data]),
+  )(source);
+};
+
+export function getAllFilesFrontMatter(folder: string): FrontMatter[] {
   const prefixPaths = path.join(root, 'data', folder);
 
   const files = getAllFilesRecursively(prefixPaths);
@@ -57,18 +83,7 @@ export async function getAllFilesFrontMatter(
     // remove drafts
     _.remove(([, { draft }]) => draft),
     // add slug and data
-    _.map(
-      ([fileName, frontmatter]): FrontMatter => ({
-        ...frontmatter,
-        slug: formatSlug(fileName),
-        date: frontmatter.date
-          ? new Date(frontmatter.date).toISOString()
-          : null,
-        lastEdit: frontmatter.lastEdit
-          ? new Date(frontmatter.date).toISOString()
-          : null,
-      }),
-    ),
+    _.map(formatFrontmatterData),
     _.sortBy((a: FrontMatter) => a.date),
     _.reverse,
   )(files);
