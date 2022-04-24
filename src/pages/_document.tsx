23471dae6a06eb014262ce1f71e5acceb0380b57
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-23 20:15:08 -0700

    feat(document): add prisma css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html style={{ height: '100vh', width: '100vw' }}>
-        <Head />
+        <Head>
+          <link href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css" rel="stylesheet" />
+        </Head>
         <body style={{ height: '100vh', width: '100vw' }}>
           {/* Make Color mode to persists when you refresh the page. */}
           <Main />
