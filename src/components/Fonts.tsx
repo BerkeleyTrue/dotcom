028b23976ef1590f7ef9d713757e39d8fb997536
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-04-23 20:14:49 -0700

    fix(Fonts): make imports absolute

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -5,7 +5,7 @@ const Fonts = () => (
     styles={`
 @font-face {
   font-family: 'Fira Code';
-  src: url('fonts/fira/FiraCode-Light.woff2') format('woff2');
+  src: url('/fonts/fira/FiraCode-Light.woff2') format('woff2');
   font-weight: 300;
   font-style: normal;
   font-display: swap;
@@ -13,7 +13,7 @@ const Fonts = () => (
 
 @font-face {
   font-family: 'Fira Code';
-  src: url('fonts/fira/FiraCode-Regular.woff2') format('woff2');
+  src: url('/fonts/fira/FiraCode-Regular.woff2') format('woff2');
   font-weight: 400;
   font-style: normal;
   font-display: swap;
@@ -21,7 +21,7 @@ const Fonts = () => (
 
 @font-face {
   font-family: 'Fira Code';
-  src: url('fonts/fira/FiraCode-Medium.woff2') format('woff2');
+  src: url('/fonts/fira/FiraCode-Medium.woff2') format('woff2');
   font-weight: 500;
   font-style: normal;
   font-display: swap;
@@ -29,7 +29,7 @@ const Fonts = () => (
 
 @font-face {
   font-family: 'Fira Code';
-  src: url('fonts/fira/FiraCode-SemiBold.woff2') format('woff2');
+  src: url('/fonts/fira/FiraCode-SemiBold.woff2') format('woff2');
   font-weight: 600;
   font-style: normal;
   font-display: swap;
@@ -37,7 +37,7 @@ const Fonts = () => (
 
 @font-face {
   font-family: 'Fira Code';
-  src: url('fonts/fira/FiraCode-Bold.woff2') format('woff2');
+  src: url('/fonts/fira/FiraCode-Bold.woff2') format('woff2');
   font-weight: 700;
   font-style: normal;
   font-display: swap;
@@ -45,7 +45,7 @@ const Fonts = () => (
 
 @font-face {
   font-family: 'Fira Code VF';
-  src: url('fonts/fira/FiraCode-VF.woff2') format('woff2-variations');
+  src: url('/fonts/fira/FiraCode-VF.woff2') format('woff2-variations');
   /* font-weight requires a range: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Fonts/Variable_Fonts_Guide#Using_a_variable_font_font-face_changes */
   font-weight: 300 700;
   font-style: normal;
