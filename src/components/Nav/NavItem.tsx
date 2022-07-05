65bb0260fd6622bcf7b054bdfeca82e5187da49d
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-07-05 12:15:23 -0500

    fix(navitem): use href prop

--- src/components/Nav/NavItem.tsx
+++ src/components/Nav/NavItem.tsx
@@ -6,13 +6,11 @@ import { O } from 'ts-toolbelt';
 import { List } from '../Lisp/List';
 import { Func } from '../Lisp/Func';
 
-type NavItemProps = PropsWithChildren<
-  O.Merge<{ href: string; icon?: ReactNode }, ButtonProps>
->;
+type NavItemProps = PropsWithChildren<O.Merge<{ href: string }, ButtonProps>>;
 
-export const NavItem = ({ icon, children, href, ...rest }: NavItemProps) => {
+export const NavItem = ({ children, href }: NavItemProps) => {
   return (
-    <NextLink href='/' passHref>
+    <NextLink href={href} passHref>
       <Button
         bg='darker.800'
         color='gray.400'
@@ -23,7 +21,6 @@ export const NavItem = ({ icon, children, href, ...rest }: NavItemProps) => {
         pl='16'
         w='100%'
         _hover={{ bg: 'gray.900', color: 'gray.200' }}
-        {...rest}
       >
         <List>
           <Func>{'=<<'}</Func>&nbsp;{children}
