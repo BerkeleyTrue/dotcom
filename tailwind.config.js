b6c845f20515d25293520cff4d70ca4c368342d1
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-03-14 13:33:22 -0600

    fix: overwrite tailwind prose theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,33 @@
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-headings": "--ctp-text",
+            color: theme("colors.text"),
+            a: {
+              color: theme("colors.sky"),
+              "&:hover": {
+                color: theme("colors.blue"),
+              },
+            },
+            "*": {
+              color: theme("colors.text"),
+            },
+            code: {
+              color: theme("colors.rosewater"),
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
     require("@catppuccin/tailwindcss")({
       defaultFlavour: "frappe",
-    })
+    }),
   ],
 }
